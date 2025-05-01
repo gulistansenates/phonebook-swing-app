428f6d65b6972c647e82cfc1eedcecbb23c19c3a
gulistansenates <gulistansenates@gmail.com>
2025-05-01 22:43:56 +0300

    feat : Add basic functionality for adding, deleting, and searching contacts in PhoneBookSwingApp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+import javax.swing.*;
+
+public static void main(String[] args) {
+    SwingUtilities.invokeLater(PhoneBookSwingApp::new);
 }
